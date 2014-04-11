6de8a23d36fc753c4e02aeb015bbdc7fb55e7718
Nick Hebner <hebnern@gmail.com>
2014-04-10 19:10:51 -0700

    Move .env linked file to web only.

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,9 +16,6 @@ set :scm, :git
 # Default value for :pty is false
 # set :pty, true
 
-# Default value for :linked_files is []
-set :linked_files, %w{.env}
-
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets}
 
--- config/deploy/web.rb
+++ config/deploy/web.rb
@@ -1,2 +1,4 @@
 
 set :repo_url, 'git@github.com:brewbit/brewbit-spree.git'
+
+set :linked_files, ['.env']
